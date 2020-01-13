e38d048944fec0c8414ce8d49a3288cc5ac8a6d9
Levi Voris <Levivoris@gmail.com>
2020-01-13 14:43:31 -0500

    Finished Terminal.cpp
    Finished Terminal.cpp
    - Need to get the blueprint now and "should" have a working in game terminal

--- Escape_Room01/Escape_Room/Source/Escape_Room/Terminal.cpp
+++ Escape_Room01/Escape_Room/Source/Escape_Room/Terminal.cpp
@@ -33,7 +33,7 @@ void UTerminal::ActiveTerminal()
 
 void UTerminal::DeactiveTerminal() const
 {
-	if (GetOwner()->InputComponent == nullprt) return;
+	if (GetOwner()->InputComponent == nullptr) return;
 
 	GetOwner()->InputComponent->KeyBindings.RemoveAt(RepeatBeingingIndex);
 	GetOwner()->InputComponent->KeyBindings.RemoveAt(PressedBindingIndex);
@@ -58,3 +58,113 @@ void UTerminal::ClearScreen()
 	UpdateText();
 }
 
+FString UTerminal::GetScreenText() const
+{
+	TArray<FString> FullTerminal = Buffer;
+	FullTerminal.Add(GPromt + InputLine);
+
+	//Wrap Lines
+	TArray<FString> WrappedLines(WrapLines(FullTerminal));
+	Truncate(WrappedLines);
+
+	return JoinWithNewline(WrappedLines);
+}
+
+TArray<FString> UTerminal::WrapLines(const TArray<FString>& Lines) const
+{
+	TArray<FString> RappedLines;
+	for (auto &&Line : Line);
+	{
+		do
+		{
+			WrappedLines.Add(CurrentLine.Left(MaxColums));
+			CurrentLine = CurrentLine.RightChop(MaxColums);
+		} 
+		while (CurrentLine.Len() > 0);
+	}
+	return WrappedLines;
+}
+
+void UTerminal::Truncate(TArray<FString>& Lines) const
+{
+	while (Lines.Num() > MaxLines)
+	{
+		Lines.RemoveAt(0);
+	}
+}
+
+FString UTerminal::JoinWithNewline(const TArray<FString>& Lines) const
+{
+	FString Results;
+	for (auto &&Line : Line)
+	{
+		Results += +TEXT(" <br> ");
+	}
+	return Result;
+}
+
+void UTerminal::OnKeyDown(FKey Key)
+{
+	if (Key == EKeys::Enter)
+	{
+		AcceptInputLine();
+	}
+
+	if (Key == EKeys::BackSpace)
+	{
+		BackSpace();
+	}
+
+	const FString KeyString = GetKeyString(Key);
+	const FModifierKeysState KeyState = FSlateApplication::Get().GetModifierKeys();
+	if (KeyState.IsShiftDown() || KeyState.AreCapsLocked())
+	{
+		InputLine += KeyString.ToUpper();
+	}
+	else
+	{
+		InputLine += KeyString.ToLower();
+	}
+
+	UpdateText();
+}
+
+
+void UTerminal::AcceptInputLine()
+{
+	Buffer.Emplace(GPromt + InputLine);
+	auto Cartridge = GetOwner()->FindComponentByClass<UCartridge>();
+	if (Cartridge != nullptr)
+	{
+		Cartridge->OnInput(InputLine);
+	}
+	InputLine = TEXT("");
+
+}
+
+void UTerminal::BackSpace()
+{
+	if (InputLine.Len() > 0)
+	{
+		InputLine.RemoveAt(InputLine.Len() - 1);
+	}
+}
+
+void UTerminal::GetKeyString(FKey Key) const
+{
+	const uint32* KeyCode = nullptr;
+	const uint32* CharCode = nullptr;
+	FInputKeyManager::Get().GetCodesFromKey(Key, KeyCode, CharCode);
+	if (CharCode != nullptr)
+	{
+		ANSICHAR Char[2] = { static_cast<ANSICHAR>(*CharCode), '\0';
+		return ANSI_TO_TCHAR(Char);
+	}
+
+		return TEXT("");
+}
+
+	void UTerminal::UpdateText()
+	{
+		TextUpdated.Broadcast(GetScreenText();
+	}
